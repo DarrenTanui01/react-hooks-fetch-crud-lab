f8af8380f6ec77103a7a32d81a1f8587af05170c
DarrenTanui01 <167513705+DarrenTanui01@users.noreply.github.com>
2025-03-05 00:31:06 +0300

    Edited QuestionList.js to pass and accept Props

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-      .then((response) => response.json())
-      .then((data) => setQuestions(data));
-  }, []);
-
+function QuestionList({ questions, onDeleteQuestion, onUpdateQuestion }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
         {questions.map((question) => (
-          <QuestionItem key={question.id} question={question} />
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDeleteQuestion={onDeleteQuestion}
+            onUpdateQuestion={onUpdateQuestion}
+          />
         ))}
       </ul>
     </section>
